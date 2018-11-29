2a029d627287ebf5a09b723e6aebe4a37bd34283
dmalys <damian.malys1@gmail.com>
2018-11-29 17:59:50 +0100

    initial commit

---
+++ Assets/MainMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+    public bool auto = false;
+    public int i = 1;
+    public int door=0;
+    public InputField inputDoor;
+    public Text ftext;
+    public void PLayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + i);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+    public void Toogle_Changed()
+    {
+        
+        if (i == 2)
+        {
+            i = 1;
+        }
+        else if (i == 1)
+        {
+            i = 2;
+        }
+    }
+    public void ChooseDoor()
+    {
+        door = 0;
+        door = int.Parse(inputDoor.text);
+        ftext.text = door.ToString();
+    }
+    
+}
---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    Animator anim;
+    bool flag;
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        flag = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player"){ 
+            if (flag)
+                anim.SetTrigger("OpenDoor");
+            anim.enabled = true;
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            anim.enabled = true;
+        }
+    }
+    void pauseAnimationEvent()
+    {
+        anim.enabled = false;
+    }
+}
